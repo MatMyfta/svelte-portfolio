29aaf031c5c4c8eb5adb298a3dbd6416b5a0b961
MatMyfta <mateomyftaraj98@gmail.com>
2024-04-13 14:12:16 +0200

    simple neobrutalism layout

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,69 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+  	import "@fontsource-variable/syne";
+	import github from "$lib/images/github.svg";
+	import linkedin from "$lib/images/linkedin.svg";
+	import mmphoto from "$lib/images/mateo_myftaraj_photo.png";
+	
+	let items = [
+	];
+	let socials = [
+		{name: 'Github', route: 'https://github.com/MatMyfta', icon: github},
+		{name: 'LinkedIn', route: 'https://www.linkedin.com/in/mateomyftaraj/', icon: linkedin},
+	]
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
+	<div class="border-b-4 bg-[#fff] py-8">
+		<div class="container mx-auto flex justify-between items-center">
+			<div class="corner">
+				<a class="logo" href="/">Mateo Myftaraj</a>
+			</div>
+		
+			<nav class="flex">
+				<ul class="flex items-center justify-between space-x-6">
+					<!-- {#each items as item}
+					<li class="flex flex-col items-center"
+						aria-current={$page.url.pathname === item.route ? 'page' : undefined}>
+						<a class="font-medium" href="{item.route}">{item.name}</a>
+					</li>
+					{/each} -->
+					<li class="flex items-center space-x-3 pb-1">
+						{#each socials as social}
+							<a class="btn-light px-2 py-2" href="{social.route}">
+								<img width="24px" height="24px" src="{social.icon}" alt="{social.name}">
+							</a>
+						{/each}
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
+	<div class="container mx-auto my-20 gap-4 grid grid-cols-7 items-center">
+		<div class="col-span-3">
+			<h1 class="font-['Syne_Variable'] font-[900] text-6xl">Ciao, sono<br>Mateo</h1>
+			<h2 class="font-['Syne_Variable'] font-[200] text-2xl">Sviluppatore Web</h2>
+			<p class="mt-4 mb-16">Something about me here</p>
+			<div class="flex gap-4">
+				<a href="mailto:mateo@myftaraj.com" class="btn bg-mainAccent">Contattami</a>
+			</div>
+		</div>
+		<div class="col-span-4 justify-self-center w-full">
+			<img class="photo-image w-full object-contain border-4" src="{mmphoto}" alt="Mateo Myftaraj in Termoli, Italy"/>
+		</div>
 	</div>
 </header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
+	.logo {
+		font-family: 'Syne Variable';
 		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
+		font-size: 22px;
 	}
 
-	a:hover {
-		color: var(--color-theme-1);
+	.photo-image {
+		border-radius: 10px;
+		box-shadow: 4px 4px var(--color-black);
 	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      bg: '#fff4e0',
+      main: '#FD9745',
+      mainAccent: '#fc7303', // not needed for shadcn
+    },
+    fontFamily: {
+      display: 'Public Sans',
+    },
     extend: {},
   },
   plugins: [],
