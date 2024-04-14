b516b15ce9e8459eab1b057f1922e43a87f6db7d
MatMyfta <mateomyftaraj98@gmail.com>
2024-04-14 11:00:10 +0200

    svelte insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import Header from './Header.svelte';
 	import './styles.css';
 	import '../app.css';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 </script>
 
 <div class="app">
