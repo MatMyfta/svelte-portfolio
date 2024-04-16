783a0b0cac0efd232c224528636b997c0210fba4
MatMyfta <mateomyftaraj98@gmail.com>
2024-04-16 22:29:27 +0200

    added empty projects page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,21 @@
 	<meta name="og:title" content="Mateo Myftaraj" />
 	<meta name="og:url" content="https://www.myftaraj.com" />
 	<meta name="og:description" content="Il portfolio e blog di Mateo Myftaraj" />
-	<meta name="og:image" content="{personalPhotoJpg}" />
+	<meta name="og:image" content={personalPhotoJpg} />
 </svelte:head>
 
-<About/>
-<Projects/>
-<Blog/>
+<About />
+
+<section class="bg-greenSoft border-y-4 w-100 py-12">
+	<div class="container mx-auto px-4">
+		<h1 class="font-[900] text-3xl lg:text-5xl text-left mb-4">Progetti</h1>
+		<Projects />
+	</div>
+</section>
+<section class="bg-violetSoft border-b-4 w-100 py-12">
+	<div class="container mx-auto px-4">
+		<h1 class="font-[900] text-3xl lg:text-5xl text-left mb-4">Blog</h1>
+		<Blog />
+	</div>
+</section>
+
