bfd8c1a89ab7b875a01c231e5f4aab51eb1d1d30
MatMyfta <mateomyftaraj98@gmail.com>
2025-02-26 21:43:16 +0100

    removed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
 	import './styles.css';
 	import '../app.css';
 	
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics'
+	// import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	// import { dev } from '$app/environment';
+	// import { inject } from '@vercel/analytics'
 
-	injectSpeedInsights();
-	inject({ mode: dev ? 'development' : 'production' });
+	// injectSpeedInsights();
+	// inject({ mode: dev ? 'development' : 'production' });
 
 </script>
 
